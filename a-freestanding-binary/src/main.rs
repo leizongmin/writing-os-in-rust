c998125170893dfb338008db91a39e540cd84b5e
Zongmin Lei <leizongmin@gmail.com>
2020-05-15 23:31:31 +0800

    a-freestanding-binary

---
+++ a-freestanding-binary/src/main.rs
@@ -0,0 +1,16 @@
+//! 参考：https://os.phil-opp.com/minimal-rust-kernel/
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
